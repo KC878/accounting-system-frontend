44b2b75d64828f1ef5749652e9776c3daf656329
Kent Chrisitan <kentcagadas19@gmail.com>
2025-10-03 08:20:16 +0800

    chore(axios): transferred api to .env.production for best practices

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -4,7 +4,7 @@ import type { UserType } from "@src/types/types"
 
 // Register a new user
 export const registerUser = async (data: UserType) => {
-  const response = await apiClient.post("api/users/register", data);
+  const response = await apiClient.post(process.env.NEXT_PUBLIC_API_USER_REGISTER!, data);
 
   return response;
 };
@@ -12,7 +12,7 @@ export const registerUser = async (data: UserType) => {
 
 // Login User
 export const loginUser = async (data: UserType) => {
-  const response = await apiClient.post("api/users/login", data, {
+  const response = await apiClient.post(process.env.NEXT_PUBLIC_API_USER_LOGIN!, data, {
     withCredentials: true, 
   });
 
